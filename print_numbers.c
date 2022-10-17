a54bced9a614533c064f5ba12cb5fdc91816a2c0
crystalxclusive <abdulsamadbayoade@gmail.com>
2022-10-17 04:00:25 +0000

    Print numbers

---
+++ print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* This program should print numbers from 0 - 20 */
+
+int main()
+{
+        int a;
+
+        for (a=0; a<=20; a++)
+                printf("\t%d", a);
+        return (0);
+}
